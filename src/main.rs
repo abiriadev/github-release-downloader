1f33d0c83bfa5ccba0f34cd307063e4ad4dde1ca
Abiria <abiria.dev@gmail.com>
2023-10-13 15:41:55 +0900

    feat: implement download logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{fs::File, io::copy};
+
 use clap::Parser;
 use github_release_downloader::models::Release;
 use inquire::{MultiSelect, Select};
@@ -19,6 +21,29 @@ fn fetch_releases(repo: &str, token: &str) -> anyhow::Result<Vec<Release>> {
 	.into_json::<Vec<Release>>()?)
 }
 
+fn download(url: &str, filename: &str) -> anyhow::Result<()> {
+	// File::open("./")
+
+	let res = ureq::get(url)
+		.set("Accept", "application/octet-stream")
+		.call()?;
+
+	let len = res
+		.header("Content-Length")
+		.expect("content length does not exist");
+
+	println!("{filename} len: {len}");
+
+	let mut reader = res.into_reader();
+	let mut f = File::open(filename)?;
+
+	copy(&mut reader, &mut f)?;
+
+	println!("{filename} done");
+
+	Ok(())
+}
+
 fn main() -> anyhow::Result<()> {
 	let cli = Cli::parse();
 
@@ -33,9 +58,11 @@ fn main() -> anyhow::Result<()> {
 		rel.assets,
 	)
 	.with_page_size(10)
-	.prompt();
+	.prompt()?;
 
-	println!("{ans:#?}");
+	ans.into_iter().for_each(|ass| {
+		download(&ass.url, &ass.name).unwrap();
+	});
 
 	Ok(())
 }
